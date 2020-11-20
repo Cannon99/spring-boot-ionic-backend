f41fa81fd073e9f93a4396e14136635c76a8923e
Thiago Furlan <thiago_furlan@outlook.com>
2020-11-20 11:32:09 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/example/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,39 @@
+package com.example.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.example.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {
+		
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getIdInteger() {
+		return id;
+	}
+
+	public void setIdInteger(Integer idInteger) {
+		this.id = idInteger;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
