26f64d3cf3a142acc8e123015c269be677bc24f4
Thiago Furlan <thiago_furlan@outlook.com>
2020-11-23 11:22:26 -0300

    Adicionando senha a Cliente

--- src/main/java/com/example/cursomc/config/SecurityConfig.java
+++ src/main/java/com/example/cursomc/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -34,22 +35,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
+		//Permitir DB H2
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
 			
+		//Ativar configurações de Cors (Cross-Origin Resource Sharing) e desabilitar CSRF (Cross-Site Request Forgery)
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
+		
+		//Assegurar que o backend não criará sessão de usuário
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
+	//Aplicar configurações básicas para acessos de múltiplas fontes em todos endpoints
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 	    return source;
 	  }
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPassEnc() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/example/cursomc/services/DBService.java
+++ src/main/java/com/example/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.cursomc.domain.Categoria;
@@ -32,6 +33,9 @@ import com.example.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPassEnc;
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -119,11 +123,11 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "O Rei", "thiagofurlan68@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "O Rei", "thiagofurlan68@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, bCryptPassEnc.encode("9999"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", c1, cli1);
-		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", c2, cli1);
+		Endereco e1 = new Endereco(null, "Rua do Rei", "900", "Apto 909", "Jardim do Rei", "38220834", c1, cli1);
+		Endereco e2 = new Endereco(null, "Avenida do Rei", "105", "Sala 999", "Centro", "38777012", c2, cli1);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
