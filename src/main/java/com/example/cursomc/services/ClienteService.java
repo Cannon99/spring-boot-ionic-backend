436da56d1866fb2f68f3774548c77d28caf8bf9f
Thiago Furlan <thiago_furlan@outlook.com>
2020-11-25 11:34:40 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.example.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -35,13 +37,19 @@ public class ClienteService {
 	private BCryptPasswordEncoder bCryptPassEnc;
 	
 	@Autowired
-	
 	private ClienteRepository repo;
+	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value ("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente find(Integer id) {
 		
@@ -127,12 +135,17 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 		
-		URI uri = s3Service.uploadFile(multipartFile);
+		BufferedImage imgJpg = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		
+		return s3Service.uploadFile(imageService.getInputStream(imgJpg, "jpg"), fileName, "image");
+		
+		/*URI uri = s3Service.uploadFile(multipartFile);
 		Cliente cli = repo.findById(user.getId()).get();
 		cli.setImageUrl(uri.toString());
 		repo.save(cli);
 		
-		return uri;
+		return uri;*/
 	}
 
 }
